95af222cc926b0d671e7d11f870ac1b327af8df0
Sumeyye Nacar <sumeyyanacar08@gmail.com>
2024-05-24 22:23:43 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args) {
+        //versiyon1
+        System.out.println("Hello Git");
+        System.out.println("git add . :staging areaya alir");
+    }
+}
