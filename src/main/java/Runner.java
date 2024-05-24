21761dc106719c793c4a56c13c2707bdc242871c
Sumeyye Nacar <sumeyyanacar08@gmail.com>
2024-05-24 22:46:12 +0300

    versiyon2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,8 @@ public class Runner {
         //versiyon1
         System.out.println("Hello Git");
         System.out.println("git add . :staging areaya alir");
+
+        //versiyon 2
+        System.out.println("git commit -m");
     }
 }
